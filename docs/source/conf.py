77c83fc6de75f072361fdc7664d930c1af1f12ee
admartin66 <admartin66@gmail.com>
2018-10-24 07:11:47 -0400

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Alex Martin'
 author = 'Alex Martin'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
