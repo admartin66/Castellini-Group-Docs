9c23c45da3a6d3a86709fad4a39665359337ae65
admartin66 <admartin66@gmail.com>
2018-10-29 23:04:26 -0400

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ release = '0.1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxcontrib.httpdomain'
+    'sphinxcontrib.httpdomain',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
